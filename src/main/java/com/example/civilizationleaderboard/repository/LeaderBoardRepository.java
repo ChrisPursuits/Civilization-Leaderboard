cb214a6bc70c9b22e613e0e8e3317639cd9f801c
chye0001 <chye0001@stud.kea.dk>
2024-07-08 11:28:04 +0200

    added get method for leaderboard in interface

--- src/main/java/com/example/civilizationleaderboard/repository/LeaderBoardRepository.java
+++ src/main/java/com/example/civilizationleaderboard/repository/LeaderBoardRepository.java
@@ -1,4 +1,8 @@
 package com.example.civilizationleaderboard.repository;
 
+import com.example.civilizationleaderboard.model.Leaderboard;
+
 public interface LeaderBoardRepository {
+
+    Leaderboard getLeaderboard(long userId);
 }
