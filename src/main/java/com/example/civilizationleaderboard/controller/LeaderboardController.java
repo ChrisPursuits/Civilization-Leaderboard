883ce53f14d9b8eb4155e1ae87c8ae62ab441f19
chye0001 <chye0001@stud.kea.dk>
2024-07-08 15:24:00 +0200

    added createLeaderboard() in leaderboardController

--- src/main/java/com/example/civilizationleaderboard/controller/LeaderboardController.java
+++ src/main/java/com/example/civilizationleaderboard/controller/LeaderboardController.java
@@ -1,4 +1,22 @@
 package com.example.civilizationleaderboard.controller;
 
+import com.example.civilizationleaderboard.dto.LeaderboardDto;
+import com.example.civilizationleaderboard.service.LeaderboardService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("leaderboard")
 public class LeaderboardController {
+
+    private final LeaderboardService leaderboardService;
+
+    public LeaderboardController(LeaderboardService leaderboardService) {
+        this.leaderboardService = leaderboardService;
+    }
+
+    @PostMapping("/create")
+    public void createLeaderboard(@RequestBody LeaderboardDto leaderboardDto) {
+        leaderboardService.createLeaderboard(leaderboardDto);
+    }
 }
