d23dc62629316eef7d5be04762e2aa2cae8a2b35
chye0001 <chye0001@stud.kea.dk>
2024-07-08 14:57:48 +0200

    added method createLeaderboard() to service

--- src/main/java/com/example/civilizationleaderboard/service/LeaderboardService.java
+++ src/main/java/com/example/civilizationleaderboard/service/LeaderboardService.java
@@ -1,4 +1,24 @@
 package com.example.civilizationleaderboard.service;
 
+import com.example.civilizationleaderboard.DtoMapper;
+import com.example.civilizationleaderboard.dto.LeaderboardDto;
+import com.example.civilizationleaderboard.model.Leaderboard;
+import com.example.civilizationleaderboard.repository.LeaderboardRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LeaderboardService {
+
+    private LeaderboardRepository leaderboardRepository;
+    private DtoMapper dtoMapper;
+
+    public LeaderboardService(LeaderboardRepository leaderboardRepository, DtoMapper dtoMapper) {
+        this.leaderboardRepository = leaderboardRepository;
+        this.dtoMapper = dtoMapper;
+    }
+
+    public void createLeaderboard(LeaderboardDto dto) {
+        Leaderboard leaderboard = dtoMapper.toLeaderboard(dto);
+        leaderboardRepository.createLeaderboard(leaderboard);
+    }
 }
