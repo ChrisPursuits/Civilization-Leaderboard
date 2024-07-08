14fdbeb13b167aed20dc718a9c0b1bcd92639c90
chye0001 <chye0001@stud.kea.dk>
2024-07-08 17:08:19 +0200

    added DTO ViewLeaderboardDto.java

---
+++ src/main/java/com/example/civilizationleaderboard/dto/ViewLeaderboardDto.java
@@ -0,0 +1,8 @@
+package com.example.civilizationleaderboard.dto;
+
+import com.example.civilizationleaderboard.model.GameStat;
+
+import java.util.List;
+
+public record ViewLeaderboardDto(String name, String description, List<GameStat> gameStatList) {
+}
