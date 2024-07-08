33c89f46a88ad9c9f21fb5c94ad7cd7346174c26
chye0001 <chye0001@stud.kea.dk>
2024-07-08 11:25:25 +0200

    created Leaderboard model

--- src/main/java/com/example/civilizationleaderboard/model/Leaderboard.java
+++ src/main/java/com/example/civilizationleaderboard/model/Leaderboard.java
@@ -1,4 +1,43 @@
 package com.example.civilizationleaderboard.model;
 
+import java.util.List;
+
 public class Leaderboard {
+
+    private String name;
+    private String description;
+    private boolean isPublic;
+    private List<GameStat> gameStatList;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean aPublic) {
+        isPublic = aPublic;
+    }
+
+    public List<GameStat> getGameStatList() {
+        return gameStatList;
+    }
+
+    public void setGameStatList(List<GameStat> gameStatList) {
+        this.gameStatList = gameStatList;
+    }
 }
