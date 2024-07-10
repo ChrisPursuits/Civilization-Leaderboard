31f3b8623fa350b8fc7085c1fa59fc9df9504eb1
chye0001 <chye0001@stud.kea.dk>
2024-07-10 10:39:41 +0200

    temporarily commented out LeaderboardController.java

--- src/main/java/com/example/civilizationleaderboard/controller/LeaderboardController.java
+++ src/main/java/com/example/civilizationleaderboard/controller/LeaderboardController.java
@@ -1,22 +1,27 @@
-package com.example.civilizationleaderboard.controller;
-
-import com.example.civilizationleaderboard.dto.LeaderboardDto;
-import com.example.civilizationleaderboard.service.LeaderboardService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("leaderboard")
-public class LeaderboardController {
-
-    private final LeaderboardService leaderboardService;
-
-    public LeaderboardController(LeaderboardService leaderboardService) {
-        this.leaderboardService = leaderboardService;
-    }
-
-    @PostMapping("/create")
-    public void createLeaderboard(@RequestBody LeaderboardDto leaderboardDto) {
-        leaderboardService.createLeaderboard(leaderboardDto);
-    }
-}
+//package com.example.civilizationleaderboard.controller;
+//
+//import com.example.civilizationleaderboard.dto.CreateLeaderboardDto;
+//import com.example.civilizationleaderboard.service.LeaderboardService;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("leaderboard")
+//public class LeaderboardController {
+//
+//    private final LeaderboardService leaderboardService;
+//
+//    public LeaderboardController(LeaderboardService leaderboardService) {
+//        this.leaderboardService = leaderboardService;
+//    }
+//
+//    @GetMapping("/{leaderboardName}")
+//    public ResponseEntity<CreateLeaderboardDto> getLeaderboardOnName() {
+//
+//    }
+//
+//    @PostMapping("/create")
+//    public void createLeaderboard(@RequestBody CreateLeaderboardDto createLeaderboardDto) {
+//        leaderboardService.createLeaderboard(createLeaderboardDto);
+//    }
+//}
