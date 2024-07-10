bdc6c16cae0be46bf130b281344bf23a6dcc5a2d
chye0001 <chye0001@stud.kea.dk>
2024-07-10 10:38:43 +0200

    added get method on leaderboard

--- src/main/java/com/example/civilizationleaderboard/repository/impl/JdbcLeaderboard.java
+++ src/main/java/com/example/civilizationleaderboard/repository/impl/JdbcLeaderboard.java
@@ -5,12 +5,14 @@ import com.example.civilizationleaderboard.model.Leaderboard;
 import com.example.civilizationleaderboard.repository.LeaderboardRepository;
 import com.example.civilizationleaderboard.service.LeaderboardService;
 import org.springframework.stereotype.Repository;
+import victoryTypeEnum.VictoryType;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -24,29 +26,64 @@ public class JdbcLeaderboard implements LeaderboardRepository {
 
     //CRUD Operations
     @Override
-    public Leaderboard getLeaderboard(long leaderboardId) {
+    public Leaderboard getLeaderboard(String leaderboardName) {
         Leaderboard leaderboard = null;
 
         try (Connection connection = dataSource.getConnection()) {
             String getLeaderboard = """
                     SELECT * FROM leaderboard
-                    WHERE id = ?;
+                    WHERE name = ?;
                     """;
             PreparedStatement preparedStatement = connection.prepareStatement(getLeaderboard);
-            preparedStatement.setLong(1, leaderboardId);
+            preparedStatement.setString(1, leaderboardName);
             ResultSet resultSet = preparedStatement.executeQuery();
 
-            if (resultSet.next()) {
+            if (resultSet.next() && resultSet.getString("name").equalsIgnoreCase(leaderboardName)) {
+                List<GameStat> gameStatList = new ArrayList<>();
+
                 leaderboard = new Leaderboard(
                         resultSet.getString(3),
                         resultSet.getString(4)
                 );
+
+                GameStat gameStat = new GameStat(
+                        resultSet.getString("name"),
+                        resultSet.getBoolean("haveWon"),
+                        resultSet.getInt("victory_points"),
+                        VictoryType.valueOf(resultSet.getString("victory_type")),
+                        resultSet.getInt("science"),
+                        resultSet.getInt("culture")
+                );
+                gameStatList.add(gameStat);
+                gameStatList = getGameStatListFromLeaderboard(resultSet, leaderboardName, gameStatList);
+
+                leaderboard.setGameStatList(gameStatList);
             }
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
 
         return leaderboard;
+    }
+
+    private List<GameStat> getGameStatListFromLeaderboard(ResultSet leaderboardRs, String leaderboardName, List<GameStat> gameStatList) {
+        try {
+            while (leaderboardRs.next() && leaderboardRs.getString("name").equalsIgnoreCase(leaderboardName)) {
+                GameStat gameStat = new GameStat(
+                        leaderboardRs.getString("name"),
+                        leaderboardRs.getBoolean("haveWon"),
+                        leaderboardRs.getInt("victory_points"),
+                        VictoryType.valueOf(leaderboardRs.getString("victory_type")),
+                        leaderboardRs.getInt("science"),
+                        leaderboardRs.getInt("culture")
+                );
+                gameStatList.add(gameStat);
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+
+        return gameStatList;
     }
 
     @Override
