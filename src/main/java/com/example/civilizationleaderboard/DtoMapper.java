7c6b864b8b1ffd16e5bd2012e7e5182b340d221e
chye0001 <chye0001@stud.kea.dk>
2024-07-10 10:41:47 +0200

    added new method to DtoMapper.java

--- src/main/java/com/example/civilizationleaderboard/DtoMapper.java
+++ src/main/java/com/example/civilizationleaderboard/DtoMapper.java
@@ -1,13 +1,22 @@
 package com.example.civilizationleaderboard;
 
-import com.example.civilizationleaderboard.dto.LeaderboardDto;
+import com.example.civilizationleaderboard.dto.CreateLeaderboardDto;
+import com.example.civilizationleaderboard.dto.ViewLeaderboardDto;
 import com.example.civilizationleaderboard.model.Leaderboard;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DtoMapper {
 
-    public Leaderboard toLeaderboard(LeaderboardDto dto) {
+    public Leaderboard toLeaderboard(CreateLeaderboardDto dto) {
         return new Leaderboard(dto.name(), dto.description());
+    }
+
+    public ViewLeaderboardDto toViewLeaderboardDto(Leaderboard leaderboard) {
+        return new ViewLeaderboardDto(
+                leaderboard.getName(),
+                leaderboard.getDescription(),
+                leaderboard.getGameStatList()
+        );
     }
 }
