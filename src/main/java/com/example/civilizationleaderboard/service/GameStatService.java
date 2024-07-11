ab17f73bf10830bc732aaa985ff5548fd9470e60
chye0001 <chye0001@stud.kea.dk>
2024-07-11 09:59:41 +0200

    populated GameStatService with logic and added create method

--- src/main/java/com/example/civilizationleaderboard/service/GameStatService.java
+++ src/main/java/com/example/civilizationleaderboard/service/GameStatService.java
@@ -1,4 +1,24 @@
 package com.example.civilizationleaderboard.service;
 
+import com.example.civilizationleaderboard.DtoMapper;
+import com.example.civilizationleaderboard.dto.CreateGameStatDto;
+import com.example.civilizationleaderboard.model.GameStat;
+import com.example.civilizationleaderboard.repository.GameStatRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GameStatService {
+
+    private GameStatRepository gameStatRepository;
+    private DtoMapper dtoMapper;
+
+    public GameStatService(GameStatRepository gameStatRepository, DtoMapper dtoMapper) {
+        this.gameStatRepository = gameStatRepository;
+        this.dtoMapper = dtoMapper;
+    }
+
+    public void createGameStat(CreateGameStatDto gameStat) {
+        GameStat gameStatToCreate = dtoMapper.toGameStat(gameStat);
+        gameStatRepository.createGameStat(gameStatToCreate);
+    }
 }
