17b208a0757d46224e2c04b25278970bda773250
chye0001 <chye0001@stud.kea.dk>
2024-07-11 10:25:59 +0200

    created new method for getting GameStat ViewDto

--- src/main/java/com/example/civilizationleaderboard/service/GameStatService.java
+++ src/main/java/com/example/civilizationleaderboard/service/GameStatService.java
@@ -2,6 +2,7 @@ package com.example.civilizationleaderboard.service;
 
 import com.example.civilizationleaderboard.DtoMapper;
 import com.example.civilizationleaderboard.dto.CreateGameStatDto;
+import com.example.civilizationleaderboard.dto.ViewGameStatDto;
 import com.example.civilizationleaderboard.model.GameStat;
 import com.example.civilizationleaderboard.repository.GameStatRepository;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,11 @@ public class GameStatService {
     public GameStatService(GameStatRepository gameStatRepository, DtoMapper dtoMapper) {
         this.gameStatRepository = gameStatRepository;
         this.dtoMapper = dtoMapper;
+    }
+
+    public ViewGameStatDto getGameStat(int gameStatId) {
+        GameStat gameStat = gameStatRepository.getGameStat(gameStatId);
+        return dtoMapper.toGameStatDto(gameStat);
     }
 
     public void createGameStat(CreateGameStatDto gameStat) {
