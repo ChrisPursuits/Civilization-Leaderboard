f883db9a651f3875b6c3407f05444704a9029fe1
chye0001 <chye0001@stud.kea.dk>
2024-07-13 11:58:26 +0200

    added 2 private helping methods for sorting players on totalWins and totalVictoryPoints. Also refactored getLeaderboard to include sorted players

--- src/main/java/com/example/civilizationleaderboard/service/LeaderboardService.java
+++ src/main/java/com/example/civilizationleaderboard/service/LeaderboardService.java
@@ -6,8 +6,15 @@ import com.example.civilizationleaderboard.dto.GameStatDto;
 import com.example.civilizationleaderboard.dto.ViewLeaderboardDto;
 import com.example.civilizationleaderboard.model.GameStat;
 import com.example.civilizationleaderboard.model.Leaderboard;
+import com.example.civilizationleaderboard.model.User;
 import com.example.civilizationleaderboard.repository.LeaderboardRepository;
+import comparator.TotalVictoryPointsComparator;
+import comparator.TotalWinsComparator;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class LeaderboardService {
@@ -27,13 +34,55 @@ public class LeaderboardService {
 
     public ViewLeaderboardDto getLeaderboard(int leaderboardId) {
         Leaderboard leaderboard = leaderboardRepository.getLeaderboard(leaderboardId);
-        return dtoMapper.toViewLeaderboardDto(leaderboard);
+        Leaderboard sortedLeaderboard = sortLeaderboard(leaderboard);
+
+        return dtoMapper.toViewLeaderboardDto(sortedLeaderboard);
     }
 
+    private Leaderboard sortLeaderboard(Leaderboard leaderboard) {
+        List<User> players = summarizePlayerStats(leaderboard);
+
+        Collections.sort(players, new TotalWinsComparator().thenComparing(new TotalVictoryPointsComparator()));
+        leaderboard.setPlayers(players);
+
+        return leaderboard;
+    }
+
+    private List<User> summarizePlayerStats(Leaderboard leaderboard) {
+        List<User> players = new ArrayList<>();
+
+        for (User player : leaderboard.getPlayers()) {
+            int totalWins = 0;
+            int totalVictoryPoints = 0;
+            int totalScience = 0;
+            int totalCulture = 0;
+
+            for (GameStat game : leaderboard.getGameStatList()) {
+                if (player.equals(game.getAccountUsername()) && game.isHaveWon()) {
+                    ++totalWins;
+                }
+
+                if (player.equals(game.getAccountUsername())) {
+                    totalVictoryPoints += game.getVictoryPoints();
+                    totalScience += game.getScience();
+                    totalCulture += game.getCulture();
+                }
+            }
+
+            player.setTotalWins(totalWins);
+            player.setTotalVictoryPoints(totalVictoryPoints);
+            player.setTotalScience(totalScience);
+            player.setTotalCulture(totalCulture);
+            players.add(player);
+        }
+
+        return players;
+    }
+
+    //FUTURE FEATURE
     public void addGameStatToLeaderboard(GameStatDto gameStatDto) {
         GameStat gameStat = dtoMapper.toGameStat(gameStatDto);
         int leaderboardId = gameStat.getLeaderboardId();
-
         leaderboardRepository.addGameStat(gameStat, leaderboardId);
     }
 }
