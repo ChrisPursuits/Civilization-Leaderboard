06dd37e0f1f61ef8781714d0c3a829178741a74c
chye0001 <chye0001@stud.kea.dk>
2024-07-13 10:06:16 +0200

    created a User model

---
+++ src/main/java/com/example/civilizationleaderboard/model/User.java
@@ -0,0 +1,68 @@
+package com.example.civilizationleaderboard.model;
+
+public class User {
+
+    private String username;
+    private String password;
+    private int totalWins;
+    private int totalVictoryPoints;
+    private int totalScience;
+    private int totalCulture;
+
+    public User(String username, String password, int totalWins, int totalVictoryPoints, int totalScience, int totalCulture) {
+        this.username = username;
+        this.password = password;
+        this.totalWins = totalWins;
+        this.totalVictoryPoints = totalVictoryPoints;
+        this.totalScience = totalScience;
+        this.totalCulture = totalCulture;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getTotalWins() {
+        return totalWins;
+    }
+
+    public void setTotalWins(int totalWins) {
+        this.totalWins = totalWins;
+    }
+
+    public int getTotalVictoryPoints() {
+        return totalVictoryPoints;
+    }
+
+    public void setTotalVictoryPoints(int totalVictoryPoints) {
+        this.totalVictoryPoints = totalVictoryPoints;
+    }
+
+    public int getTotalScience() {
+        return totalScience;
+    }
+
+    public void setTotalScience(int totalScience) {
+        this.totalScience = totalScience;
+    }
+
+    public int getTotalCulture() {
+        return totalCulture;
+    }
+
+    public void setTotalCulture(int totalCulture) {
+        this.totalCulture = totalCulture;
+    }
+}
