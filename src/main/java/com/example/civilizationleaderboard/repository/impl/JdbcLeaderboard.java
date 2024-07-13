1e72b78066e899d08335ed77d778a7e61dc9f2d7
chye0001 <chye0001@stud.kea.dk>
2024-07-13 11:07:19 +0200

    refactored getLeaderboard() to reflect changes in leaderboard model

--- src/main/java/com/example/civilizationleaderboard/repository/impl/JdbcLeaderboard.java
+++ src/main/java/com/example/civilizationleaderboard/repository/impl/JdbcLeaderboard.java
@@ -2,6 +2,7 @@ package com.example.civilizationleaderboard.repository.impl;
 
 import com.example.civilizationleaderboard.model.GameStat;
 import com.example.civilizationleaderboard.model.Leaderboard;
+import com.example.civilizationleaderboard.model.User;
 import com.example.civilizationleaderboard.repository.LeaderboardRepository;
 import org.springframework.stereotype.Repository;
 import victoryTypeEnum.VictoryType;
@@ -36,29 +37,18 @@ public class JdbcLeaderboard implements LeaderboardRepository {
             ResultSet resultSet = preparedStatement.executeQuery();
 
             if (resultSet.next()) {
-                List<GameStat> gameStatList = new ArrayList<>();
-
                 leaderboard = new Leaderboard(
                         resultSet.getString(2),
                         resultSet.getString(3)
                 );
 
-                if (resultSet.getString(8) != null) {
-                    GameStat gameStat = new GameStat(
-                            resultSet.getInt(5),
-                            resultSet.getString(6),
-                            resultSet.getInt(7),
-                            resultSet.getString(8),
-                            resultSet.getBoolean(9),
-                            resultSet.getInt(11),
-                            VictoryType.valueOf(resultSet.getString(10)),
-                            resultSet.getInt(12),
-                            resultSet.getInt(13)
-                    );
-                    gameStatList.add(gameStat);
-                    gameStatList = getGameStatListFromLeaderboard(resultSet, gameStatList);
+                if (resultSet.getString(6) != null) {
+                    List<GameStat> gameStatList = getGameStatListFromLeaderboard(resultSet);
+                    List<User> players = getAllPlayersFromLeaderboard(leaderboardId);
 
+                    leaderboard.setPlayers(players);
                     leaderboard.setGameStatList(gameStatList);
+
                     return leaderboard;
                 }
             }
@@ -69,19 +59,72 @@ public class JdbcLeaderboard implements LeaderboardRepository {
         return leaderboard;
     }
 
-    private List<GameStat> getGameStatListFromLeaderboard(ResultSet leaderboardRs, List<GameStat> gameStatList) {
+    private List<User> getAllPlayersFromLeaderboard(int leaderboardId) {
+        List<User> players = new ArrayList<>();
+
+        try (Connection connection = dataSource.getConnection()){
+            try {
+                connection.setAutoCommit(false);
+
+                String getAllPlayers = """
+                        SELECT DISTINCT account_username FROM leaderboard l
+                        LEFT JOIN game_stat gs ON gs.leaderboard_id = l.id
+                        WHERE l.id = ?;
+                        """;
+
+                PreparedStatement preparedStatement = connection.prepareStatement(getAllPlayers);
+                preparedStatement.setInt(1, leaderboardId);
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                while (resultSet.next()) {
+                    User player = new User(resultSet.getString(1));
+                    players.add(player);
+                }
+
+                connection.commit();
+                connection.setAutoCommit(true);
+
+            }catch (SQLException sqlException) {
+                connection.rollback();
+                connection.setAutoCommit(true);
+                sqlException.printStackTrace();
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+
+        return players;
+    }
+
+    private List<GameStat> getGameStatListFromLeaderboard(ResultSet leaderboardsRs) {
+        List<GameStat> gameStatList = new ArrayList<>();
         try {
-            while (leaderboardRs.next()) {
-                GameStat gameStat = new GameStat(
-                        leaderboardRs.getInt(5),
-                        leaderboardRs.getString(6),
-                        leaderboardRs.getInt(7),
-                        leaderboardRs.getString(8),
-                        leaderboardRs.getBoolean(9),
-                        leaderboardRs.getInt(11),
-                        VictoryType.valueOf(leaderboardRs.getString(10)),
-                        leaderboardRs.getInt(12),
-                        leaderboardRs.getInt(13)
+            //The courser has already been moved down the list once at the if check,
+            //therefore we will have to instance GameStat, before running the while-loop
+            GameStat gameStat = new GameStat(
+                    leaderboardsRs.getInt(5),
+                    leaderboardsRs.getString(6),
+                    leaderboardsRs.getInt(7),
+                    leaderboardsRs.getString(8),
+                    leaderboardsRs.getBoolean(9),
+                    leaderboardsRs.getInt(11),
+                    VictoryType.valueOf(leaderboardsRs.getString(10)),
+                    leaderboardsRs.getInt(12),
+                    leaderboardsRs.getInt(13)
+            );
+            gameStatList.add(gameStat);
+
+            while (leaderboardsRs.next()) {
+                gameStat = new GameStat(
+                        leaderboardsRs.getInt(5),
+                        leaderboardsRs.getString(6),
+                        leaderboardsRs.getInt(7),
+                        leaderboardsRs.getString(8),
+                        leaderboardsRs.getBoolean(9),
+                        leaderboardsRs.getInt(11),
+                        VictoryType.valueOf(leaderboardsRs.getString(10)),
+                        leaderboardsRs.getInt(12),
+                        leaderboardsRs.getInt(13)
                 );
                 gameStatList.add(gameStat);
             }
