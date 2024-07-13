882ecb08b4d7beed68a3f9549d654ff74fd56374
chye0001 <chye0001@stud.kea.dk>
2024-07-13 11:58:41 +0200

    added attribute players to Dto

--- src/main/java/com/example/civilizationleaderboard/dto/ViewLeaderboardDto.java
+++ src/main/java/com/example/civilizationleaderboard/dto/ViewLeaderboardDto.java
@@ -1,11 +1,13 @@
 package com.example.civilizationleaderboard.dto;
 
 import com.example.civilizationleaderboard.model.GameStat;
+import com.example.civilizationleaderboard.model.User;
 
 import java.util.List;
 
 public record ViewLeaderboardDto(
         String name,
         String description,
-        List<GameStat> gameStatList) {
+        List<GameStat> gameStatList,
+        List<User> players) {
 }
