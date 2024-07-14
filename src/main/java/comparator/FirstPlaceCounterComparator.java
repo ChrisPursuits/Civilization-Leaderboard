ad6c136adef56347a6c9425fb7c685ea5c6a24fa
chye0001 <chye0001@stud.kea.dk>
2024-07-14 10:58:55 +0200

    renamed TotalWinsComparator.java to FirstPlaceCounterComparator.java

--- src/main/java/comparator/FirstPlaceCounterComparator.java
+++ src/main/java/comparator/FirstPlaceCounterComparator.java
@@ -4,9 +4,9 @@ import com.example.civilizationleaderboard.model.User;
 
 import java.util.Comparator;
 
-public class TotalWinsComparator implements Comparator<User> {
+public class FirstPlaceCounterComparator implements Comparator<User> {
     @Override
     public int compare(User u1, User u2) {
-        return Integer.compare(u1.getTotalWins(), u2.getTotalWins());
+        return Integer.compare(u1.getFirstPlaceCount(), u2.getFirstPlaceCount());
     }
 }
